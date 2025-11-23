4b384a0ac6b9f9691104c2a2f2ccbb1b6aacb539
ms-solly <theworldofthoughtsyo@google.com>
2025-11-23 21:50:08 +0530

    removed functions

--- main.cpp
+++ main.cpp
@@ -15,21 +15,6 @@ typedef struct App{
 	VkInstance instance;
 
 }App;
-//skeleton functions
-void init_window(App *app);
-void create_instance(App *app);
-
-void init_vulkan(App *app);
-void main_loop(App *app);
-void cleanup(App *app);
-
-// resource functions
-VkInstanceCreateInfo res_instance(App app);
-
-
-
-
-
 
 // 
 bool check_extensions_supported() {
@@ -60,12 +45,22 @@ bool check_extensions_supported() {
 
 
 
-VkInstanceCreateInfo res_instance(){
+int main() {
+	App app;
+	assert(glfwInit() && "glfw initialisation failed\n" );
+	printf("glfw initialized successfully\n");
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	app.window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+	assert(app.window && "window creation failed\n");
+	printf("window created successfully\n");
+	assert(check_extensions_supported() && "not all necessary glfw extensions are supported");
+	printf("all necessary extensions are supported\n");
 	uint32_t glfwExtensionCount = 0;
 	const char** glfwExtensions;
 	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 	uint32_t instance_version = VK_API_VERSION_1_0;
-	vkEnumerateInstanceVersion(&instance_version);
+	//vkEnumerateInstanceVersion(&instance_version);
 	VkApplicationInfo app_info = {
 		.sType                   = VK_STRUCTURE_TYPE_APPLICATION_INFO,
 		.pApplicationName        = "Hello Vulkan",
@@ -83,62 +78,19 @@ VkInstanceCreateInfo res_instance(){
 		.ppEnabledExtensionNames = glfwExtensions,
 
 	};
-	return create_info;
-
-}
-
-void run(App *app) {
-	init_window(app);
-        init_vulkan(app);
-        main_loop(app);
-        cleanup(app);
-
-}
-void init_window(App *app){
-	assert(glfwInit() && "glfw initialisation failed\n" );
-	printf("glfw initialized successfully\n");
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-	app->window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
-	assert(app->window && "window creation failed\n");
-	printf("window created successfully\n");
-}
-void create_instance(App *app){
-	assert(check_extensions_supported() && "not all necessary glfw extensions are supported");
-	printf("all necessary extensions are supported\n");
-	VkInstanceCreateInfo create_info = res_instance();
-	VkResult result = vkCreateInstance(&create_info, nullptr, &app->instance);
+	VkResult result = vkCreateInstance(&create_info, nullptr, &app.instance);
 	//assert(result && "instance creation failed\n");
 	assert(result == VK_SUCCESS && "instance creation failed\n");
 	printf("instance created successfully\n");
-}
-void init_vulkan(App *app) {
-	create_instance(app);
-}
-
-void main_loop(App *app) {
-	while (!glfwWindowShouldClose(app->window)) {
+	while (!glfwWindowShouldClose(app.window)) {
 		glfwPollEvents();
 	}
-}
 
-void cleanup(App *app) {
-	glfwDestroyWindow(app->window);
+	vkDestroyInstance(app.instance, nullptr);
+	glfwDestroyWindow(app.window);
 
 
 	glfwTerminate();
-}
-
-int main() {
-	App app;
-
-	try {
-		run(&app);
-	} catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-		return EXIT_FAILURE;
-	}
-
 	return EXIT_SUCCESS;
 }
 
